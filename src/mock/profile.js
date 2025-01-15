59fed90541f7ba024cc5d72e972ee7fccc43e15d
cannonball008 <nicothomson659@gmail.com>
2025-01-14 23:03:03 -0500

    portfolio

---
+++ src/mock/profile.js
@@ -0,0 +1,23 @@
+export const SOCIAL_LINKS = [
+  {
+    link: 'https://x.com/nico99815',
+    icon: 'akar-icons:twitter-fill',
+  },
+  {
+    link: 'https://github.com/nico99815',
+    icon: 'akar-icons:github-fill',
+  },
+  {
+    link: 'https://t.me/nico99815',
+    icon: 'akar-icons:telegram-fill',
+  },
+];
+
+export const HERO_TITLES = ['Nico Thomson', 'Full-stack & Blockchain Developer', 'AI Enthusiast'];
+
+export const aboutParagraph =
+  'Highly motivated and enthusiastic Full Stack & Blockchain Developer with experience in designing, developing and maintaining Web2 & Web3 platforms using technologies such as Solidity, Rust, FunC, Tact, Huskell, React, Next.js, React-Native, Vue and etc';
+
+export const contactEmail = 'nicothomson659@gmail.com';
+
+export const documentTitle = 'Hello I am Nico Thomson | Full Stack & Blockchain Developer | AI Enthusiast';
---
+++ src/mock/tech-skills.js
@@ -0,0 +1,266 @@
+export const FRONTEND_SKILLS = [
+  {
+    label: 'HTML',
+    proficiency: '80%',
+    icon: 'vscode-icons:file-type-html',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'CSS',
+    proficiency: '80%',
+    icon: 'vscode-icons:file-type-css',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Javascript',
+    proficiency: '90%',
+    icon: 'logos:javascript',
+    iconClasses: 'text-2xl',
+  },
+  {
+    label: 'Typescript',
+    proficiency: '80%',
+    icon: 'logos:typescript-icon',
+    iconClasses: 'text-2xl',
+  },
+  {
+    label: 'React.js',
+    proficiency: '80%',
+    icon: 'vscode-icons:file-type-reactjs',
+    iconClasses: 'text-2xl',
+  },
+  {
+    label: 'Next.js',
+    proficiency: '90%',
+    icon: 'cib:next-js',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Angular.js',
+    proficiency: '90%',
+    icon: 'cib:angular',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Vue.js',
+    proficiency: '90%',
+    icon: 'logos:vue',
+    iconClasses: 'text-2xl',
+  },
+  {
+    label: 'Gatsby',
+    proficiency: '90%',
+    icon: 'logos:gatsby',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Material UI (mui)',
+    proficiency: '90%',
+    icon: 'logos:material-ui',
+    iconClasses: 'text-2xl',
+  },
+  {
+    label: 'Tailwind CSS',
+    proficiency: '90%',
+    icon: 'logos:tailwindcss-icon',
+    iconClasses: 'text-2xl',
+  },
+  {
+    label: 'SCSS',
+    proficiency: '90%',
+    icon: 'vscode-icons:file-type-scss2',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Bootstrap',
+    proficiency: '90%',
+    icon: 'logos:bootstrap',
+    iconClasses: 'text-2xl',
+  },
+
+  {
+    label: 'Redux',
+    proficiency: '90%',
+    icon: 'logos:redux',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'React Router',
+    proficiency: '90%',
+    icon: 'logos:react-router',
+    iconClasses: 'text-xl',
+  },
+  {
+    label: 'React Native',
+    proficiency: '75%',
+    icon: 'logos:react',
+    iconClasses: 'text-xl',
+  },
+  {
+    label: 'Flutter',
+    proficiency: '70%',
+    icon: 'logos:flutter',
+    iconClasses: 'text-xl',
+  },
+  {
+    label: 'React Query',
+    proficiency: '90%',
+    icon: 'logos:react-query-icon',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'React Hook Form',
+    proficiency: '90%',
+    icon: 'fluent:form-20-regular',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Lodash',
+    proficiency: '90%',
+    icon: 'logos:lodash',
+    iconClasses: 'text-xl',
+  },
+];
+
+export const BACKEND_SKILLS = [
+  {
+    label: 'Node.js',
+    proficiency: '90%',
+    icon: 'vscode-icons:file-type-node',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Express (Node.js Framework)',
+    proficiency: '90%',
+    icon: 'simple-icons:express',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Java',
+    proficiency: '90%',
+    icon: 'logos:java',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Python',
+    proficiency: '90%',
+    icon: 'logos:python',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'REST Api',
+    proficiency: '90%',
+    icon: 'dashicons:rest-api',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Graphql',
+    proficiency: '90%',
+    icon: 'logos:graphql',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Solidity',
+    proficiency: '90%',
+    icon: 'logos:solidity',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Rust',
+    proficiency: '90%',
+    icon: 'logos:rust',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'FunC',
+    proficiency: '90%',
+    icon: 'ph:function-duotone',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Prisma',
+    proficiency: '90%',
+    icon: 'vscode-icons:file-type-light-prisma',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Sequelize',
+    proficiency: '90%',
+    icon: 'vscode-icons:file-type-sequelize',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'PostgreSQL',
+    proficiency: '90%',
+    icon: 'vscode-icons:file-type-pgsql',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'MySQL',
+    proficiency: '90%',
+    icon: 'logos:mysql',
+    iconClasses: 'text-2xl',
+  },
+  {
+    label: 'Mongodb',
+    proficiency: '90%',
+    icon: 'vscode-icons:file-type-mongo',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'AWS',
+    proficiency: '80%',
+    icon: 'logos:aws',
+    iconClasses: 'text-xl',
+  },
+  {
+    label: 'Github',
+    proficiency: '80%',
+    icon: 'akar-icons:github-fill',
+    iconClasses: 'text-3xl',
+  },
+  {
+    label: 'Bitbucket',
+    proficiency: '80%',
+    icon: 'logos:bitbucket',
+    iconClasses: 'text-2xl',
+  },
+  {
+    label: 'Docker',
+    proficiency: '80%',
+    icon: 'logos:docker-icon',
+    iconClasses: 'text-2xl',
+  },
+  {
+    label: 'Heroku',
+    proficiency: '80%',
+    icon: 'logos:heroku-icon',
+    iconClasses: 'text-2xl',
+  },
+  {
+    label: 'Vercel',
+    proficiency: '80%',
+    icon: 'logos:vercel-icon',
+    iconClasses: 'text-2xl',
+  },
+];
+
+export const FRONTEND_DESCRIPTIONS = [
+  'Building complex, interactive user interfaces using React, React-Native, Vue, Angular, Next.js',
+  'Integration Smart Contract and Front-end by using web3.js',
+  'Building Large scale Web applications using state management library',
+  'Optimizing performances by minimizing HTTP requests, reducing file sizes, implementing caching techniques',
+  'Good collaborating with designers and backend developers to integrate front-end interfaces with backend systems and APIs',
+  'Ensuring cross-browser compatibility and Code debugging and troubleshooting',
+  'Building responsive Web/Mobile Applications and Staying updated with industry trends',
+];
+
+export const BACKEND_DESCRIPTIONS = [
+  'Writing Smart Contract in Solidity, Rust, Move, FunC on EVM chains, Solana, CosmWasm, Aptos, Sui, Cardano and TON.',
+  'Developing server-side logic to handle data processing, authentication, security, and business logic',
+  'Building RESTful APIs and GraphQL APIs that allow different components of a web application to communicate with each other',
+  'Working with databases to design and optimize database schemas, write complex queries, and ensure data integrity and performance',
+  'Implementing security measures to handle encryption, authentication, authorization, and data validation',
+  'Integrating with third-party services and APIs such as payment gateways, social media platforms, and cloud services',
+  'Optimizing the performance and scalability of back-end systems by implementing caching mechanisms, load balancing, and database query optimizations',
+];
